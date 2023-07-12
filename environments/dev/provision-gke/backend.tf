75fe983862d8cc8709bd3fbb9291903077f2c98e
fmbah <fmbah@westminster.gov.uk>
2023-07-12 16:25:43 +0100

    gh action

--- environments/dev/provision-gke/backend.tf
+++ environments/dev/provision-gke/backend.tf
@@ -1,7 +1,7 @@
 # Using backend config in CLI will override this block
 terraform {
   backend "gcs" {
-    bucket = "dm-poc-development"
+    bucket = "dm-poc-dev-remote"
     prefix = "environments/dev/gcs_state.tfstate"
   }
 }
